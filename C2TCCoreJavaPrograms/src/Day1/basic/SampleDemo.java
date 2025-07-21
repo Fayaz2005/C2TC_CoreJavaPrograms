19824b525cd790a12c6da1c5440403de3e942503
Lenovo <Lenovo@LAPTOP-L9Q5JUTU>
2025-07-21 12:18:11 +0530

    This is my first project

---
+++ C2TCCoreJavaPrograms/src/Day1/basic/SampleDemo.java
@@ -0,0 +1,11 @@
+package Day1.basic;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to JES....");
+
+	}
+
+}
