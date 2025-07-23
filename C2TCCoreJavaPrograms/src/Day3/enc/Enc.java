7e8eea2c10eca468c702723b5bb023b8df62c942
Lenovo <Lenovo@LAPTOP-L9Q5JUTU>
2025-07-23 23:27:31 +0530

    encapsulation

--- C2TCCoreJavaPrograms/src/Day3/enc/Enc.java
+++ C2TCCoreJavaPrograms/src/Day3/enc/Enc.java
@@ -1,5 +1,15 @@
 package Day3.enc;
 
 public class Enc {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
 
 }
