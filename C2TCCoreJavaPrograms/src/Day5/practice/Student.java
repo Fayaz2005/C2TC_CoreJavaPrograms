116761622a6d949e22b2a548c8b9204df72e7fa2
Lenovo <Lenovo@LAPTOP-L9Q5JUTU>
2025-07-25 16:12:52 +0530

    association of day Five

---
+++ C2TCCoreJavaPrograms/src/Day5/practice/Student.java
@@ -0,0 +1,25 @@
+package Day5.practice;
+
+public class Student extends Person{
+	private int uid;
+	private String sname;
+	private String courseName;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}h
+}
